875b1683bfa7a3ef4371490d07953d1a270e2eb5
Brigid <brigidbook@gmail.com>
2022-10-12 20:37:20 -0500

    incomplete

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/user/add")]
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.error = false;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.username = newUser.Username;
+            if (newUser.Password != verify)
+            {
+                ViewBag.password = newUser.Password;
+                ViewBag.error = true;
+                return Redirect("/user/add");
+            }
+            return View("Index");
+        }
+    }
+}
